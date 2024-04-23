b88a947580c367fb08611ada8d7fcc6a6e46b774
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2024-04-22 21:43:33 -0300

    Refactor

--- src/main/java/br/com/rafaedudu/calculadoradecrafts/CalculadoraDeCraftsApplication.java
+++ src/main/java/br/com/rafaedudu/calculadoradecrafts/CalculadoraDeCraftsApplication.java
@@ -2,10 +2,6 @@ package br.com.rafaedudu.calculadoradecrafts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class CalculadoraDeCraftsApplication {
